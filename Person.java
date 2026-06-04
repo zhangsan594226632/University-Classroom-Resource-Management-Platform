8d2bd887fb2238f774cdbcd8a2b62d27dd5114b1
熊猫IT技术 <44674276+zhangsan594226632@users.noreply.github.com>
2026-06-04 15:57:38 +0800

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,24 @@
+package com.baidu.day01;//源文件的名字一般大写开头，名字要和public修饰的类的名字一致！
+//Java严格区分大小写
+
+/**
+ @author bruce
+ @version v5
+ */
+public class Person {
+
+    public static void main(String[] args) {
+        //这是一个标题
+        System.out.println("静夜思");
+        /*
+          这是一首诗的内容
+          表达了李白的之情。
+        */
+        System.out.println("窗前明月光，疑是地上霜。");
+    }
+}
+//一个Java源文件中可以写多个类，但是只能有一个public修饰的类
+
+class Dog{
+
+}
---
+++ Student.java
@@ -0,0 +1,10 @@
+package com.baidu.day01;
+
+public class Student {
+
+    public static void main(String[] args) {
+
+        int age=18;
+
+    }
+}
